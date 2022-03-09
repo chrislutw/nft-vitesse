6f08916f50e03bd123586f35db134ee953ba16a4
chrislutw <kshke0723@gmail.com>
2022-03-09 10:29:36 +0800

    change config and add nft test component

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 import type { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare const Unity: any
